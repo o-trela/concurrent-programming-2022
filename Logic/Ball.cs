77baa712a280847361390458a06e3f5083a0c565
trela677 <236677@edu.p.lodz.pl>
2022-04-13 14:35:06 +0200

    Ball class created and tested.

---
+++ Logic/Ball.cs
@@ -0,0 +1,16 @@
+﻿namespace Logic
+{
+    public class Ball
+    {
+        public int Radius { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        public Ball(int radius, int posX, int posY)
+        {
+            Radius = radius;
+            PosX = posX;
+            PosY = posY;
+        }
+    }
+}
