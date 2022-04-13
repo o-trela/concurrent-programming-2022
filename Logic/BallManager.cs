deb526dfcd2417930220044c80d2c66fef2308ac
trela677 <236677@edu.p.lodz.pl>
2022-04-13 16:09:54 +0200

    BallManager created and tested. Begining form.

---
+++ Logic/BallManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Logic
+{
+    public class BallManager
+    {
+        private int _boardHeight;
+        private int _boardWidth;
+        private int _ballRadius;
+        private Ball[] balls;
+
+        public BallManager(int boardHeight, int boardWidth, int ballRadius)
+        {
+            _boardHeight = boardHeight;
+            _boardWidth = boardWidth;
+            _ballRadius = ballRadius;
+        }
+
+        public Ball[] RandomBallCreation(int count)
+        {
+            balls = new Ball[count];
+            int x, y;
+
+            for (int i = 0; i < count; i++)
+            {
+                (x, y) = GetRandomPos();
+                balls[i] = new(_ballRadius, x, y);
+            }
+
+            return balls;
+        }
+
+        private (int, int) GetRandomPos()
+        {
+            Random rand = Random.Shared;
+
+            int x = rand.Next(0 + _ballRadius, _boardWidth - _ballRadius);
+            int y = rand.Next(0 + _ballRadius, _boardHeight - _ballRadius);
+
+            return (x, y);
+        }
+    }
+}
