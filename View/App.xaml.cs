7a3a8fd3e4b14136f28ad7874b0aa2e9a7b9c521
kirsz559 <236559@edu.p.lodz.pl>
2022-04-20 19:46:59 +0200

    Namespace fix

--- View/App.xaml.cs
+++ View/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace View
+namespace BallSimulator.Presentation.View
 {
     /// <summary>
     /// Interaction logic for App.xaml
