48607e59c1133944090b4e31d3749576197edff7
kirsz559 <236559@edu.p.lodz.pl>
2022-04-20 23:15:11 +0200

    Extensions and ball.Move

--- Logic/Ball.cs
+++ Logic/Ball.cs
@@ -1,11 +1,12 @@
-﻿namespace BallSimulator.Logic
+﻿using System;
+
+namespace BallSimulator.Logic
 {
     public class Ball
     {
         public int Radius { get; private set; }
         public Vector2 Position { get; set; }
-        public float Speed { get; set; }
-        public float Direction { get; set; } // [0, 1] where 0 and 1 is right, 0.25 is down...
+        public Vector2 Speed { get; set; }
 
         public Ball(int radius, int posX, int posY)
             : this(radius, new Vector2(posX, posY))
@@ -15,6 +16,12 @@
         {
             Radius = radius;
             Position = position;
+        }
+
+        public void Move(float strength)
+        {
+            if (!strength.Between(0f, 1f)) throw new ArgumentException("Strenght must be between 0 and 1!", nameof(strength));
+            Position += Speed * strength;
         }
     }
 }
---
+++ Logic/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BallSimulator.Logic
+{
+    public static class Extension
+    { 
+        public static bool Between(this float f, float a, float b)
+        {
+            return (f > a) && (f < b);
+        }
+
+        public static bool Between(this int f, int a, int b)
+        {
+            return (f > a) && (f < b);
+        }
+    }
+}
